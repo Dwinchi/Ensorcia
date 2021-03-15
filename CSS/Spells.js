82fae6be3cfe82213510559b8d8fdc3f332a4078
Dwinchi <edwinnerchi@gmail.com>
2021-03-15 10:18:28 -0500

    Fixed better menu

--- CSS/Spells.js
+++ CSS/Spells.js
@@ -18,3 +18,6 @@ function openElement(evt, elementName) {
   document.getElementById(elementName).style.display = "block";
   evt.currentTarget.className += " active";
 }
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
